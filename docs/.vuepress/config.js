6d274c0c66c7ab37d4758e8c4c013b778a1826e3
kira <2439004149@qq.com>
2020-09-22 20:35:55 +0800

    http缓存相关总结

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -67,6 +67,9 @@ module.exports = {
             'HTML',
             'CSS',
             'VUE基础',
+            'React',
+            'Wepback',
+            'Http',
             '常见考点',
           )
         }
@@ -92,11 +95,11 @@ module.exports = {
 }
 
 
-function getBasicsSidebar(groupA, groupB, groupC, groupD,groupE) {
+function getBasicsSidebar(groupA, groupB, groupC, groupD,groupE, groupF,groupG) {
   return [{
       title: groupA,
       collapsable: false,
-      children: ['/about/basics/事件流','/about/basics/数据类型', '/about/basics/闭包','/about/basics/异步', '/about/brower/事件循环', '/about/basics/arguments','/about/basics/promise']
+      children: ['/about/basics/事件流','/about/basics/数据类型','/about/basics/new的过程','/about/basics/原型链', '/about/basics/闭包','/about/basics/异步', '/about/brower/事件循环', '/about/basics/arguments','/about/basics/promise']
     },
     {
       title: groupB,
@@ -116,7 +119,17 @@ function getBasicsSidebar(groupA, groupB, groupC, groupD,groupE) {
     {
       title: groupE,
       collapsable: true,
-      children: ['/about/question/vue',]
+      children: ['/about/webpack/webpack配置',]
     },
+    {
+      title: groupF,
+      collapsable: true,
+      children: ['/about/webpack/webpack配置',]
+    },
+    {
+      title: groupG,
+      collapsable: true,
+      children: ['/about/http/缓存',]
+    }
   ]
 }
