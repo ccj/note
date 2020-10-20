45e5b74032bd583aa8c0c0075c8b2dc164e3dc73
kira <978647751@qq.com>
2020-10-20 17:52:02 +0800

    安全&&浏览器加载过程

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -104,7 +104,7 @@ function getBasicsSidebar(groupA, groupB, groupC, groupD,groupE, groupF,groupG,g
   return [{
       title: groupA,
       collapsable: false,
-      children: ['/about/basics/事件流','/about/basics/数据类型','/about/basics/数组操作','/about/basics/new的过程','/about/basics/原型链', '/about/basics/闭包','/about/basics/异步', '/about/brower/事件循环', '/about/basics/arguments','/about/basics/promise','/about/basics/ts学习']
+      children: ['/about/basics/事件流','/about/basics/数据类型','/about/basics/数组操作','/about/basics/new的过程','/about/basics/原型链', '/about/basics/闭包','/about/basics/异步', '/about/brower/事件循环','/about/brower/渲染过程', '/about/basics/arguments','/about/basics/promise','/about/basics/ts学习']
     },
     {
       title: groupB,
@@ -134,7 +134,7 @@ function getBasicsSidebar(groupA, groupB, groupC, groupD,groupE, groupF,groupG,g
     {
       title: groupG,
       collapsable: true,
-      children: ['/about/http/缓存',]
+      children: ['/about/http/缓存','/about/http/web安全']
     },
     {
       title: groupH,
